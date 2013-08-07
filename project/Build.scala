fb4972e6571393e1a3551db9767fabed5cdae9d3
Rick Snyder <rsnyder@anypresence.com>
2013-08-07 15:36:40 -0400

    Add support for publishing artifacts to github-hosted maven repo

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
 import play.Project._
+import com.github.hexx.GithubRepoPlugin._
 
 object ApplicationBuild extends Build {
 
@@ -8,14 +9,14 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
   )
-
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+
+    localRepo := Path.userHome / "github" / "play_api_maven_repo",
+
+    githubRepo := "git@github.com:AnyPresence/play_api_maven_repo.git"
+
+  ).settings(githubRepoSettings: _*)
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,7 +2,12 @@
 logLevel := Level.Warn
 
 // The Typesafe repository 
-resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+resolvers ++= Seq(
+	"Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+	"fuzion24-releases" at "http://fuzion24.github.io/maven/releases"
+)
 
-// Use the Play sbt plugin for Play projects
 addSbtPlugin("play" % "sbt-plugin" % "2.1.3")
+
+addSbtPlugin("com.github.hexx" % "sbt-github-repo" % "0.1.0")
+
